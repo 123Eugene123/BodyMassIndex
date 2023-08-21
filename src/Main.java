849f06a3defb4fb8809625bc7dabfa49f6dba8ca
Eugene test <eupushkash@ngs.ru>
2023-08-21 22:53:00 +0300

    Change Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int myIndex = service.calculate(58, 1.59);
+        System.out.println(myIndex);
     }
+}
